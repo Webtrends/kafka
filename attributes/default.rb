5b016b42bdf263e973ce71dc00b24ea81cfd158e
John S. Jacob <jsjacob@ticketfly.com>
2013-12-16 14:28:42 -0800

    Update for Kafka 0.8.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,9 +17,9 @@
 # limitations under the License.
 
 # Install
-default[:kafka][:version] = "0.7.1"
+default[:kafka][:version] = "0.8.0"
 default[:kafka][:download_url] = nil
-default[:kafka][:checksum] = "ee845b947b00d6d83f51a93e6ff748bb03e5945e4f3f12a77534f55ab90cb2a8"
+default[:kafka][:checksum] = "ecadd6cf9f59e22444af5888c8b9595c5652ffab597db038418e85dfa834062e"
 
 default[:kafka][:install_dir] = "/opt/kafka"
 default[:kafka][:data_dir] = "/var/kafka"
